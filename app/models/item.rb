ce8e0799bcdc13fc4aa441f5fbd6114a3b05fab4
marikonemoto <marikonemoto4@gmail.com>
2020-12-30 18:55:41 +0900

    ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,18 @@
 class Item < ApplicationRecord
-  belongs_to: user
-  has_one: order
-  belongs_to_active_hash: category
-  belongs_to_active_hash: condition
-  belongs_to_active_hash: shipping_cost
-  belongs_to_active_hash: area
-  belongs_to_active_hash: time_to_ship
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :area
+  belongs_to_active_hash :time_to_ship
+
+  belongs_to :user
+  has_one :order
+  has_one_attached :image, dependent: :destroy
+
+  validates :image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :area_id, :time_to_ship_id, :price, presence: true
+
+  validates :category_id, :condition_id, :shipping_cost_id, :area_id, :time_to_ship_id, numericality: { only_integer: true } 
+    
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は適正な販売価格を入力してください' }  
 end
