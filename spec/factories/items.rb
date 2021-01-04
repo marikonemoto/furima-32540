2c69f39cfbeaca11546d7aa2b3b48f3afaa36670
marikonemoto <marikonemoto4@gmail.com>
2021-01-04 15:39:23 +0900

    app/factories/items.rbの編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
   factory :item do
     name             { Faker::Food.fruits }
     description      { '商品の説明' }
-    category_id      { '1' }
-    condition_id     { '1' }
-    shipping_cost_id { '1' }
-    area_id          { '1' }
-    time_to_ship_id  { '1' }
+    category_id      { 1 }
+    condition_id     { 1 }
+    shipping_cost_id { 1 }
+    area_id          { 1 }
+    time_to_ship_id  { 1 }
     price            { Faker::Number.within(range: 300..9_999_999) }
-    user_id          { '1' }
+    user_id          { 1 }
     association :user
   end
 end
