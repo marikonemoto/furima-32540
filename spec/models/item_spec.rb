ed04b5b855a10d08e2bcbe2078932eaef6594c4e
marikonemoto <marikonemoto4@gmail.com>
2021-01-04 14:39:56 +0900

    テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,88 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('public/images/test_image.png.jpg')
+  end
+
+  describe '商品出品登録' do
+    context '商品出品登録がうまくできた場合' do
+      it 'name, description, category_id, condition_id, shipping_cost_id, area_id, time_to_ship_id, price, user_idが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'imageが存在する場合は登録できる' do
+        expect(@item.image).to be_valid
+      end
+    end
+
+    context '商品出品登録がうまくできない場合' do
+      it 'imageが空だと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空だと登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idが空だと登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが空だと登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'shipping_cost_idが空だと登録できない' do
+        @item.shipping_cost_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+      it 'area_idが空だと登録できない' do
+        @item.area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'time_to_ship_idが空だと登録できない' do
+        @item.time_to_ship_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Time to ship can't be blank")
+      end
+      it 'priceが空だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number", "Price Out of setting range")
+      end
+      it 'priceが300未満の場合は登録できない' do
+        @item.price = '250'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+      it 'priceが9999999より大きい場合は登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Out of setting range")
+      end
+      it 'priceが半角数字以外の場合は保存できない' do
+        @item.price = '７７７'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
+
+#販売価格は半角数字のみ保存可能であること
+#入力された販売価格によって、販売手数料や販売利益の表示が変わること
+#必要な情報を適切に入力すると、商品の出品ができること
+#エラーハンドリングができていること（適切では無い値が入力された場合、情報は保存されず、エラーメッセージを出力させること）
+#入力に問題がある状態で出品ボタンが押されたら、出品ページに戻りエラーメッセージが表示されること
