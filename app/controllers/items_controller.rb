7c30354dd57a886e5e3b730a53649f63f97a10c7
marikonemoto <marikonemoto4@gmail.com>
2021-01-05 21:41:10 +0900

    URLを直接入力を阻止する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -44,4 +45,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def correct_user
+    @item = current_user.items.find_by(id: params[:id])
+      unless @item
+        redirect_to root_path
+      end
+  end
 end
