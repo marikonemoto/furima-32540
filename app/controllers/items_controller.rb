bdfc85da67bad7018f05c97943a2f4bbba7641f7
marikonemoto <marikonemoto4@gmail.com>
2021-01-05 21:01:17 +0900

    Controllerの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -19,14 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
@@ -35,4 +39,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :price, :description, :category_id, :condition_id, :shipping_cost_id, :area_id,
                                  :time_to_ship_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
