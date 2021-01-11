6136b92a65553f3fff087a7b37520b0dbb536f65
marikonemoto <marikonemoto4@gmail.com>
2021-01-11 12:20:34 +0900

    フォームの情報を保存できる

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @user_order = UserOrder.new  
+  end
+ 
+  def create
+    @user_order = UserOrder.new(order_params)   
+     if @user_order.valid?
+       @user_order.save
+       redirect_to root_path
+     else
+       render 'orders/index'
+     end
+  end
+ 
+  private
+   
+  def order_params
+   params.require(:user_order).permit(:postal_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+ 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+end
